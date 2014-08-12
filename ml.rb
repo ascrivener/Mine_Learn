d22060559d1e1c78c63ca82b019b60eed512b56c
Adam Scrivener <aascrivener@gmail.com>
2014-08-12 16:38:49 -0400

    Added better 'GUI', and a method in Board to get all confs for a given tile

--- ml.rb
+++ ml.rb
@@ -1,11 +1,11 @@
 class Board
-	attr_accessor :board, :height, :width, :num_bombs, :deducer
+	attr_accessor :board, :height, :width, :num_bombs
 	def initialize(height=10, width=10, num_bombs=10)		
 		@height = height
 		@width = width
 		@board = Array.new(height) {Array.new(width,nil)}
 		@num_bombs = num_bombs
-		@deducer = Deducer.new
+		@dirs = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
 
 		(0..@height-1).each do |i|
 			(0..@width-1).each do |j|
@@ -23,15 +23,13 @@ class Board
 			end
 		end
 
-		dirs = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
-
 		(0..@height-1).each do |i|
 			(0..@width-1).each do |j|
 				if @board[i][j].has_bomb
 					@board[i][j].number = -1
 				else
 					count = 0
-					dirs.each do |d_i,d_j|
+					@dirs.each do |d_i,d_j|
 						t_i = i+d_i
 						t_j = j+d_j
 						if (inbounds(t_i,t_j))
@@ -61,17 +59,14 @@ class Board
 			puts "already flipped!"
 		else
 			@board[i][j].flipped = true
-			# if (@board[i][j].number == 0)
-			# 	clear(i,j)
-			# else
-				#update(i,j)
-			#end
+			gen_confs(i,j)
+			if (@board[i][j].number == 0)
+				clear(i,j)
+			end
 		end
 	end
 	def clear(i,j)
-		dirs = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
-
-		dirs.each do |d_i,d_j|
+		@dirs.each do |d_i,d_j|
 			t_i = i+d_i
 			t_j = j+d_j
 
@@ -86,9 +81,43 @@ class Board
 	def inbounds(i,j)
 		return (i >=0 && i < @height && j >= 0 && j < @width)
 	end
+	def gen_confs(i,j)
+		tiles = []
+		confs = []
+
+		@dirs.each do |d_i,d_j|
+			t_i = i+d_i
+			t_j = j+d_j
+			if (inbounds(t_i,t_j) && !board[t_i][t_j].flipped)
+				tiles << [t_i,t_j]
+			end
+		end
+
+		list = Chooser.new(tiles.size,@board[i][j].number).confs
+
+		list.each do |c_list|
+			a=[]
+			c_list.each_with_index do |elm,k|
+				if (elm == 1)
+					a << tiles[k]
+				end
+			end
+			confs << a
+		end
+		@board[i][j].confs = confs
+		puts "confs for #{i}, #{j}: #{confs.inspect}"
+	end
 	def out(final=false)
+		print "\n   "
+		(0..(@width-1)).each do |i|
+			print "#{i} "
+		end
+		print "\n\n"
 		(0..(@height-1)).each do |i|
 			(0..(@width-1)).each do |j|
+				if j == 0
+					print "#{i}  "
+				end
 				print "#{@board[i][j].out(final)} "
 			end
 			puts
@@ -98,11 +127,13 @@ class Board
 end
 
 class Tile
-	attr_accessor :flipped, :marked, :has_bomb, :number
+	attr_accessor :flipped, :marked, :has_bomb, :number, :confs
 	def initialize(has_bomb=false)
 		@flipped = false
 		@marked = false
 		@has_bomb = has_bomb
+		@number = -1
+		@confs = nil
 	end
 	def out(final=false)
 		if @flipped
@@ -118,31 +149,31 @@ class Tile
 end
 
 class Chooser
-	attr_accessor :a
+	attr_accessor :confs
 	def initialize(n,k)
 		@a = Array.new(n+1){Array.new(k+1,nil)}
 		gen_confs(n,k)
 		#puts a[n][k].inspect
-		puts a[n][k].size
+		@confs = @a[n][k]
+		remove_instance_variable(:@a)
 	end
 	def gen_confs(n,k)
-		puts "#{n} #{k}"
-		arr = a[n][k]
+		arr = @a[n][k]
 		if (arr)
 			# puts "already exists"
 			return arr
 		elsif (k==0)
-			a[n][k] = [[0]*n]
+			@a[n][k] = [[0]*n]
 			# puts "k is 0, made into #{a[n][k]}"
-			return a[n][k]
+			return @a[n][k]
 		elsif (n==k)
-			a[n][k] = [[1]*n]
+			@a[n][k] = [[1]*n]
 			# puts "n = k, made into #{a[n][k]}"
-			return a[n][k]
+			return @a[n][k]
 		else
-			a[n][k] = gen_confs(n-1,k-1).map{|x| x + [1]} + gen_confs(n-1,k).map{|x| x + [0]}
+			@a[n][k] = gen_confs(n-1,k-1).map{|x| x + [1]} + gen_confs(n-1,k).map{|x| x + [0]}
 			# puts "made into #{a[n][k]}"
-			return a[n][k]
+			return @a[n][k]
 		end
 
 	end
@@ -167,7 +198,5 @@ class Game
 	end
 end
 
-Chooser.new(25,12)
 
-
-# Game.new.play
+Game.new.play
